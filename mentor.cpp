02e2242f9e00699bbf434d8e732743e403f12366
Kiel <kiel.friedt@gmail.com>
2011-03-10 18:55:15 -0800

    modified:   mentor.cpp

--- mentor.cpp
+++ mentor.cpp
@@ -1,13 +1,6 @@
 /*
+ Mentor Graphics interview code
  Adding a integer element to an link list, no duplicates and the array is sorted smalled to largest.
- 
- Cases: Adding integer 10 to the list
- 1) head -> null
- 2) head -> 2 -> null
- 3) head -> 2 -> 10 
- 4) head -> 11 -> 12
- 5) head -> 8 -> 11
- 
  This is quick code for an interview with Mentor Graphics, if more time was given i would make classes.
  */
 
@@ -16,59 +9,37 @@ struct node {
 	node * next;
 }
 
-
 int main(int argc, char * const argv[]){
-	int value = 10;
-	node * head = NULL;
-	node * ptr;
-	node * ptr2;
-	if (head == NULL) {
+	node * tempptr = new node;
+	tempptr ->val = value;
+	tempptr->next = null;
+	
+	node * ptr2 = head;
+	node * ptr = NULL;
+	
+	if(head == NULL){
 		head = new node;
-		head -> val = value
-		head -> next = NULL;
-	}else {
-		ptr = head;
-		ptr2 = head -> next;
-		while (ptr2 != NULL){
-			if(ptr -> val < value){
-				if(ptr2 == NULL) // ptr < value but ptr equals null
-				{
-					ptr2 = new node;
-					ptr2 -> val = value;
-					ptr2 -> next = NULL;
-					break;
-				}else if (ptr2 -> val != value) {  //ptr < value and ptr != value, move on
-					ptr = ptr -> next;
-					ptr2 = ptr -> next;
-					continue;
-				}else if( ptr2 -> val == value) //ptr2 -> val equals value
-					break;
-			}else if( ptr -> val == value){  //ptr -> val equals value
+		head->val = value;
+		head->next = NULL;
+	}
+	else{
+		while(ptr2 != null)
+		{
+			if(ptr2->val >= tempptr->val)
 				break;
-			}else{  // ptr -> val > value
-				ptr -> next = new node;
-				int temp = ptr -> val;
-				ptr -> val = value;
-				ptr = ptr -> next;
-				ptr -> val = temp;
-				ptr -> next = ptr2;
-			}
+			ptr = ptr2;
+			ptr2 = ptr2->next;
+		}
+		if(ptr == null)
+		{
+			tempptr->next = head;
+			head = tempptr;
+		}
+		else if(ptr2->val != value)
+		{
+			ptr2->next = tempptr;
 		}
 	}
-	if (ptr2 == NULL) {
-		ptr2 = new node;
-		ptr2 -> val = value;
-		ptr2 -> next = NULL;
-	}
+	
+	return 1;
 }
-
-
-
-
-
-
-
-return 1;
-}
-
-
